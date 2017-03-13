39941a85ebb73fcbe66f309becfbd5c2b248e4ec
Erica Wright and Maggie Harrington <erica@>
2017-03-13 13:16:14 -0700

    linked open weather api

--- build/js/app.js
+++ build/js/app.js
@@ -80,4 +80,16 @@ $(document).ready(function() {
   $('#time').text(moment());
 });
 
+var apiKey = 'b2277cb20bcd24ac17786d778be3abce';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val('');
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text('The humidity in ' + city + ' is ' + response.main.humidity + '%');
+    });
+  });
+});
+
 },{"./../js/journal.js":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -21,3 +21,15 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('#time').text(moment());
 });
+
+var apiKey = 'b2277cb20bcd24ac17786d778be3abce';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val('');
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text('The humidity in ' + city + ' is ' + response.main.humidity + '%');
+    });
+  });
+});
